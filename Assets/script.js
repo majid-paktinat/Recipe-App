13de85aad67cdf13a6b639034d8729acc9f3cc36
majid <majid.paktinat@gmail.com>
2020-02-14 14:56:24 -0500

    files added

---
+++ Assets/script.js
@@ -0,0 +1,76 @@
+
+let nutritionArr = [];
+let analyzedInstructionsArr = [];
+
+let imageAdd;
+let mealImage
+
+let settings = {
+	"async": true,
+	"crossDomain": true,
+    "method": "GET",
+    "url": "https://api.spoonacular.com/recipes/complexSearch?minFat=1&maxFat=50&minCarbs=5&maxCarbs=100&number=5&type=salad&addRecipeInformation=true&apiKey=b588453d88164a63a6b235e77276dcd0"
+    //"url": "https://api.spoonacular.com/recipes/582897/nutritionWidget?apiKey=54f37f7c8cf54dabb996147b296b7f34&defaultCss=true" //$('body').html(response);
+    
+	}
+
+
+$.ajax(settings).then(getResponse);
+
+$.ajax(settings).fail(getError);
+
+
+function getResponse( response ){
+    console.log(`<.Then> callback <${response}>`);console.log(response);
+    
+    
+    let resultsCounter = 0;
+    while (resultsCounter<response.results.length){
+
+        console.log(`Id is : ${response.results[resultsCounter].id}`);
+        console.log(`Title is : ${response.results[resultsCounter].title}`);
+        console.log(`Dish Types : ${response.results[resultsCounter].dishTypes}`);
+        
+        nutritionArr = response.results[resultsCounter].nutrition; 
+        let nutritionCounter = 0;
+        while(nutritionCounter<nutritionArr.length){
+            console.log(`Nutrition Title : ${nutritionArr[nutritionCounter].title}`); 
+            console.log(`Nutrition Amount : ${nutritionArr[nutritionCounter].amount}`); 
+            console.log(`Nutrition Unit : ${nutritionArr[nutritionCounter].unit}`); 
+            console.log("--nutrition--");
+            nutritionCounter = nutritionCounter + 1;
+        }
+
+        console.log(`Servings : ${response.results[resultsCounter].servings}`);
+        analyzedInstructionsArr = response.results[resultsCounter].analyzedInstructions; 
+        let analyzedInstructionsCounter=0;
+        let stepsCounter = 0;
+        while (analyzedInstructionsCounter<analyzedInstructionsArr.length){
+            while(stepsCounter<analyzedInstructionsArr[analyzedInstructionsCounter].steps.length){
+                console.log(`analyzedInstructions Name : ${analyzedInstructionsArr[analyzedInstructionsCounter].name}`); 
+                console.log(`analyzedInstructions Steps(number) : ${analyzedInstructionsArr[analyzedInstructionsCounter].steps[stepsCounter].number}`);
+                console.log(`analyzedInstructions Steps(step) : ${analyzedInstructionsArr[analyzedInstructionsCounter].steps[stepsCounter].step}`);
+                stepsCounter = stepsCounter + 1;
+            }
+            console.log("---------------------------------------");
+            analyzedInstructionsCounter = analyzedInstructionsCounter + 1;
+            stepsCounter = 0;
+        }
+
+        
+        imageAdd = response.results[resultsCounter].image; 
+        mealImage = $('<img />');
+        mealImage.attr("src", imageAdd);
+        $('#meal-image-view').append(mealImage);
+        
+        resultsCounter = resultsCounter + 1;
+    }
+
+    
+    
+   
+}
+function getError( errorStatus ) {
+    console.log(`<.Fail> callback <${errorStatus}>`);
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Test
+# Test# Project 1 : Search-For-Recipes
+github address : https://github.com/xenetto/SearchRecipes
+
+## api keys:
+``` 
+Majid : b588453d88164a63a6b235e77276dcd0
+Sahar : 7be734db65ca420487a133024e247177
+Bowen : 54f37f7c8cf54dabb996147b296b7f34
+```
+
+## References:
+```
+
+https://api.spoonacular.com/recipes/complexSearch?minFat=1&maxFat=50&number=5&type=salad&addRecipeInformation=true&apiKey=...
+
+https://api.spoonacular.com/food/ingredients/autocomplete?query=appl&number=5&apiKey=...
+
+https://api.spoonacular.com/recipes/{id}/nutritionWidget?&defaultCss=trueapiKey=...
+
+https://spoonacular.com/food-api/docs#Diets
+https://spoonacular.com/food-api/docs#Cuisines
+https://spoonacular.com/food-api/docs#Meal-Types
+https://spoonacular.com/food-api/docs#Intolerances
+https://spoonacular.com/food-api/docs#Search-Recipes-Complex
+
+```
+
+## inputs:
+    includeIngredients
+    excludeIngredients
+    type // type=salad
+    diet
+    intolerances
+    cuisine
+    number // of result
+    addRecipeInformation=true // if result includes recipes
+
+## outputs:
+    id
+    title
+    dishTypes
+    nutrition.title
+    nutrition.amount
+    nutrition.unit
+    analyzedInstructions.name
+    analyzedInstructions.number
+    analyzedInstructions.step
+    image
+    serving
